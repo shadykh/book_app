f7feac62d0dd70e992e7ede636eeae2e97941534
shadykh <eng.shady94@gmail.com>
2021-04-14 21:27:48 +0300

    lab13, finshed-Localy

--- data/book.sql
+++ data/book.sql
@@ -3,6 +3,8 @@ CREATE TABLE books (
   id SERIAL PRIMARY KEY,
   title VARCHAR(255),
   authors VARCHAR(255),
+  isbn VARCHAR(255),
   imageLinks VARCHAR(255),
-  description TEXT
+  description TEXT,
+  categories VARCHAR(255)
 );
