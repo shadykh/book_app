eccdbcea4869489b3ad2fcbbbac0986505f7d2d8
shadykh <eng.shady94@gmail.com>
2021-04-14 09:07:44 +0300

    lab12, Without CSS

--- server.js
+++ server.js
@@ -24,20 +24,16 @@ server.set('view engine', 'ejs');
 
 
 server.get('/', renderIndex);
-// (req, res) => {
-//     res.render('pages/index');
-// })
-
 
 server.get('/searches/new', (req, res) => {
     res.render('pages/searches/new');
 });
 
-server.post('/books/:arrayIdx', insertBook);
-
-server.post('/books/show/:bookIdx', moreAboutBook);
-
 server.post('/searches', handleNewSerach);
+
+server.post('/books', insertBook);
+
+server.get('/books/:id', moreAboutBook);
 
 
 // Functions
@@ -46,7 +42,6 @@ function renderIndex(req, res) {
     let SQL = `SELECT * FROM books;`;
     client.query(SQL)
         .then(results => {
-            console.log(results, 'hhhhh');
             res.render('pages/index', { taskResults: results.rows })
         })
         .catch(err => {
@@ -55,8 +50,6 @@ function renderIndex(req, res) {
 }
 
 function handleNewSerach(req, res) {
-
-    //console.log(req.body, 'the req');
 
     let serachWord = req.body.searchWord;
 
@@ -71,6 +64,7 @@ function handleNewSerach(req, res) {
 
     superagent.get(googleBooksURL)
         .then(googleBooksData => {
+
             // console.log('------------------------------insideSuper-------------------')
             // console.log('title', googleBooksData.body.items[0].volumeInfo.title);
             // console.log('authors', googleBooksData.body.items[0].volumeInfo.authors);
@@ -90,39 +84,45 @@ function handleNewSerach(req, res) {
         })
 
         .catch(error => {
-            console.log('Error in getting data from Google Books server')
-            console.error(error);
+            //console.log('Error in getting data from Google Books server')
+            // console.error(error);
             res.render('pages/error', { errors: error });
         })
 }
-//http://localhost:4042/books/0
-function insertBook(req, res) {
 
-    let { title, authors, imageLinks, description } = Book.all[req.params.arrayIdx];
+
+//http://localhost:4042/books
+function insertBook(req, res) {
+    //console.log(req.body);
+    let { title, authors, imageLinks, description } = Book.all[req.body.idx];
     let SQL = `INSERT INTO books (title, authors, imageLinks, description) VALUES ($1,$2,$3,$4) RETURNING *;`;
     let safeValues = [title, authors, imageLinks, description];
     client.query(SQL, safeValues)
         .then(result => {
-            console.log(result.rows)
-            res.redirect(`/books/show/${result.rows[0].id}`)
+            //console.log(result.rows[0].id)
+            res.redirect(`/books/${result.rows[0].id}`)
         })
 }
 
-function moreAboutBook(req, res) {
-    console.log(req);
 
-    // let SQL = `SELECT * FROM books;`;
-    // client.query(SQL)
-    //     .then(results => {
-    //         console.log(results, 'hhhhh');
-    //         res.render('pages/index', { taskResults: results.rows })
-    //     })
-    //     .catch(err => {
-    //         res.render('pages/error', { errors: err });
-    //     })
+
+function moreAboutBook(req, res) {
+    console.log(req.params.id);
+    let SQL = `SELECT * FROM books where id=${req.params.id};`;
+    client.query(SQL)
+        .then(results => {
+            //console.log(results.rows, 'hhhhh');
+            res.render('pages/books/show', { taskResults: results.rows })
+        })
+        .catch(err => {
+            res.render('pages/error', { errors: err });
+        })
 }
 
+
+
 function Book(title, authors, imageLinks, description) {
+
     this.title = (title) ? title : 'There is no availble title';
     this.authors = (authors) ? authors.join(', ') : 'There is no availble authors';
     this.imageLinks = (imageLinks) ? imageLinks.thumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
